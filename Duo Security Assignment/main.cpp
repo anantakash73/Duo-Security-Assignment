7a941bad185fd8102efa1aabdb04ca69f6b9ba16
Akash <aanant@mojo8.ccs.itd.umich.edu>
2015-01-30 19:04:10 -0500

    Initial Commit

---
+++ Duo Security Assignment/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Duo Security Assignment
+//
+//  Created by Akash, Anant on 1/30/15.
+//  Copyright (c) 2015 Akash, Anant. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
